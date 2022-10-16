7981be678990fd400c936f632185ada8a3e069a4
Naïm Elmaftouhi <naim.elmaftouhi@gmail.com>
2022-10-16 22:51:12 +0200

    Remove unused comments

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "styled-components";
 
 import "./index.css";
@@ -19,11 +18,9 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        {/*<PersistGate persistor={persistor}>*/}
         <BrowserRouter>
           <App />
         </BrowserRouter>
-        {/*  </PersistGate>*/}
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
